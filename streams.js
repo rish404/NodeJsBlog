df16f1722701198253addb15fab54795b6bba020
rish404 <rish.destiny@gmail.com>
2021-06-21 16:48:14 +0530

    Added streams

--- streams.js
+++ streams.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt');
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+//Read Stream
 
 readStream.on('data', (chunk) => {
     console.log('-------- NEW CHUNK ------')
     console.log(chunk);
+    writeStream.write('\nNEW CHUNK\n')
+    writeStream.write(chunk);
 })
+
+//Piping
+
+readStream.pipe(writeStream);
