f035034d4cd47f3bd49b9fa7f3472fcbba1da7c8
rish404 <rish.destiny@gmail.com>
2021-06-21 22:34:11 +0530

    created routing system for different html pages

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
-    // res.setHeader('Content-Type', 'text/html');
-    // res.write('<h1>hello, ninjas</h1>');
-    // res.write('<p>hello again, ninjas</p>');
-    // res.end();
-
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break; 
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
     //send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err){
             console.log(err);
             res.end();
