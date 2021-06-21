428fc5399d39d91ab48a2ac7e5921e5922e9fb15
rish404 <rish.destiny@gmail.com>
2021-06-21 22:21:19 +0530

    added views folder

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
 
-
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>hello, ninjas</h1>');
+    res.write('<p>hello again, ninjas</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () =>{
