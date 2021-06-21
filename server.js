0b25ad827d325804efa2278ec03cee96f238d080
rish404 <rish.destiny@gmail.com>
2021-06-21 22:25:05 +0530

    import external html file contents to server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1>hello, ninjas</h1>');
+    // res.write('<p>hello again, ninjas</p>');
+    // res.end();
+
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>hello, ninjas</h1>');
-    res.write('<p>hello again, ninjas</p>');
-    res.end();
+
+    //send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+
+    })
+    
 });
 
 server.listen(3000, 'localhost', () =>{
